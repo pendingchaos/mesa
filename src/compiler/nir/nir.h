ce9eaab4a2c677c468c67bcc98f85eeeeea0ba10
Daniel Schürmann <daniel.schuermann@campus.tu-berlin.de>
2018-05-22 10:07:53 +0200

    nir: better divergence analysis differenciating phi types

--- src/compiler/nir/nir.h
+++ src/compiler/nir/nir.h
@@ -3160,6 +3160,8 @@ void nir_to_lcssa(nir_shader *shader);
  */
 bool nir_convert_from_ssa(nir_shader *shader, bool phi_webs_only);
 
+bool* nir_divergence_analysis(nir_shader *shader);
+
 bool nir_lower_phis_to_regs_block(nir_block *block);
 bool nir_lower_ssa_defs_to_regs_block(nir_block *block);
 bool nir_rematerialize_derefs_in_use_blocks_impl(nir_function_impl *impl);
