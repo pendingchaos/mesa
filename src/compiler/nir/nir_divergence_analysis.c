cda42bbda272dcd0e0441848d9a67bcef9a7965c
Rhys Perry <pendingchaos02@gmail.com>
2019-04-23 10:39:46 +0100

    amd/compiler: implement nir_intrinsic_quad_*

--- src/compiler/nir/nir_divergence_analysis.c
+++ src/compiler/nir/nir_divergence_analysis.c
@@ -202,6 +202,10 @@ visit_intrinsic(bool *divergent, nir_intrinsic_instr *instr, unsigned subgroup_s
    case nir_intrinsic_shared_atomic_exchange:
    case nir_intrinsic_shared_atomic_comp_swap:
    case nir_intrinsic_shuffle:
+   case nir_intrinsic_quad_broadcast:
+   case nir_intrinsic_quad_swap_horizontal:
+   case nir_intrinsic_quad_swap_vertical:
+   case nir_intrinsic_quad_swap_diagonal:
    default:
       is_divergent = true;
       break;
